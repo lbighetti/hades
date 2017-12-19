1df84d8d09a310ea095bce58ca89b7c897fb3cae
Leandro Bighetti <lbighetti@gmail.com>
2017-12-19 13:25:44 +0100

    Nanobox config for dev.exs
    * Need to get database credentials from environment variables

--- config/dev.exs
+++ config/dev.exs
@@ -39,8 +39,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :hades, Hades.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "",
+  username: System.get_env("DATA_DB_USER"),
+  password: System.get_env("DATA_DB_PASS"),
+  hostname: System.get_env("DATA_DB_HOST"),
   database: "hades_dev",
-  hostname: "localhost",
   pool_size: 10
