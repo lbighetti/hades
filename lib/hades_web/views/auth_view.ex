bbc60f6f9e558da805c76b711677233a2e2a1a31
Cristiano Carvalho <cristiano.codelab@gmail.com>
2017-12-23 20:44:57 -0300

    Minor improvements

--- lib/hades_web/views/auth_view.ex
+++ lib/hades_web/views/auth_view.ex
@@ -2,9 +2,10 @@ defmodule HadesWeb.AuthView do
   use HadesWeb, :view
 
   def render("auth.json", %{user: user}) do
-    %{id: user.id,
-      email: user.email,
-      name: user.name,
-      is_admin: user.is_admin}
+    %{data:
+        %{id: user.id,
+          email: user.email,
+          name: user.name,
+          is_admin: user.is_admin}}
   end
 end
