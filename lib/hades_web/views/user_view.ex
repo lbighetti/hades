25e901ef2a40f5d5e48dfaefcda84f6a61d616c8
Cristiano Carvalho <cristiano.codelab@gmail.com>
2017-12-24 12:10:58 -0300

    Fix missing attr on user view

--- lib/hades_web/views/user_view.ex
+++ lib/hades_web/views/user_view.ex
@@ -8,6 +8,6 @@ defmodule HadesWeb.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id, email: user.email, is_admin: user.is_admin}
+    %{id: user.id, email: user.email, name: user.name, is_admin: user.is_admin}
   end
 end
