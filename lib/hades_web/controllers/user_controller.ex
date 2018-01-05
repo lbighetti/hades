10292e80a2bef52770d62659bf29697bd4e8a5bd
Cristiano Carvalho <cristiano.codelab@gmail.com>
2018-01-04 22:00:51 -0300

    Implement update user endpoint (#51)

--- lib/hades_web/controllers/user_controller.ex
+++ lib/hades_web/controllers/user_controller.ex
@@ -8,7 +8,16 @@ defmodule HadesWeb.UserController do
   def get_user(conn, %{"id" => id}) do
     with {:ok, %User{} = user} <- Users.get_user!(id) do
       conn
-      |> put_status(200)
+      |> put_status(:ok)
+      |> render("show.json", user: user)
+    end
+  end
+
+  def update_user(conn, %{"user" => user_params}) do
+    current_user = Hades.Guardian.Plug.current_resource(conn)
+    with {:ok, %User{} = user} <- Users.update_user(current_user, user_params) do
+      conn
+      |> put_status(:ok)
       |> render("show.json", user: user)
     end
   end
